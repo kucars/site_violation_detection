855812fc8c251721897c11800709f8320ac1f5d2
Yasmeen <100039613@kustar.ac.ae>
2015-01-29 13:47:42 +0400

    - fix code errors

--- scripts/site_violation_detection.py
+++ scripts/site_violation_detection.py
@@ -28,29 +28,24 @@ class image_converter:
 
     (rows,cols,channels) = cvImage.shape
     
-    #cv2.circle(cvImage, (cols/2,rows/2), 225, (255,0,0),2)
-
-    #cv2.putText(cvImage,"Robotics Institute", (50, rows / 2), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255),2)
-    # cv2.putText(cvImage,"Khalifa University", (50, rows / 2 + 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0),2)
-    #cv2.imshow("images", image1)
     image2 = cv2.cvtColor(cvImage,cv2.COLOR_BGR2HSV)
     # define the list of boundaries
-    	# create NumPy arrays from the boundaries
     lower = np.array([110, 50, 50])
     upper = np.array([130, 255, 255])
 	# find the colors within the specified boundaries and apply
 	# the mask
     mask = cv2.inRange(image2, lower, upper)
     output = cv2.bitwise_and(image2, image2, mask = mask)
-    im3 = image1.copy()
+    im3 = cvImage.copy()
 	# show the images
     if mask.any() != 0:
       cv2.putText(im3,"violation detected", (50, rows / 2 + 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255),2)
       cv2.imshow('frame_blue',im3)
-      cv2.imwrite('violation_detected_human_present.png', im3)
+      #cv2.imwrite('violation_detected_human_present.png', im3)
       cv2.imshow('mask_blue',mask)
     else:
       cv2.imshow('frame_blue',cvImage)
+    
     im = cvImage.copy()
     # convert the image to grayscale, blur it, and detect edges
     gray = cv2.cvtColor(im.copy(), cv2.COLOR_BGR2GRAY)
