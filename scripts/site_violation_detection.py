a20ac488a6d01baacb68bfb5086ac8abe93530f2
Yasmeen <100039613@kustar.ac.ae>
2015-01-29 18:59:56 +0400

    addded adoptive thresholding

--- scripts/site_violation_detection.py
+++ scripts/site_violation_detection.py
@@ -3,7 +3,6 @@ import roslib
 roslib.load_manifest('site_violation_detection')
 import sys
 import rospy
-import argparse
 import cv2
 import numpy as np
 from std_msgs.msg import String
@@ -16,6 +15,7 @@ class image_converter:
   def __init__(self):
     rospy.init_node('site_violation_detection', anonymous=True)
     self.image_pub = rospy.Publisher("/site_violation_detection/image_raw",Image, queue_size=10)
+    cv2.namedWindow("Image window", 1)
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber("/web_cam/image_raw",Image,self.callback)
     
@@ -26,38 +26,43 @@ class image_converter:
       print e
 
     (rows,cols,channels) = cvImage.shape
-    
+    # convert to hsv color space
     image2 = cv2.cvtColor(cvImage,cv2.COLOR_BGR2HSV)
-    # define the list of boundaries
+    # define the list of boundaries for blue color
     lower = np.array([110, 50, 50])
     upper = np.array([130, 255, 255])
 	# find the colors within the specified boundaries and apply
 	# the mask
-    mask   = cv2.inRange(image2, lower, upper)
+    mask = cv2.inRange(image2, lower, upper)
     output = cv2.bitwise_and(image2, image2, mask = mask)
     im3 = cvImage.copy()
 	# show the images
     if mask.any() != 0:
       cv2.putText(im3,"violation detected", (50, rows / 2 + 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255),2)
-    
+      #cv2.imshow('frame_blue',im3)
+      #cv2.imshow('mask_blue',mask)
+    #else:
+      #cv2.imshow('frame_blue',cvImage)
     im = cvImage.copy()
-    # convert the image to grayscale, blur it, and detect edges
-    gray  = cv2.cvtColor(im.copy(), cv2.COLOR_BGR2GRAY)
-    edged = cv2.Canny(gray, 35, 125)
+    # convert the image to grayscale
+    gray = cv2.cvtColor(im.copy(), cv2.COLOR_BGR2GRAY)
+    th3 = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,15,11)
+    edged = cv2.Canny(th3, 35, 100)
     # find the contours in the edged image and keep the largest one;
     (cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     c = max(cnts, key = cv2.contourArea)
-    
     # compute the bounding box of the of the paper region and return it
     k=cv2.minAreaRect(c)
     width = min(k[1])
     length = max(k[1])
     area = width*length 
-    print area 
     x,y,w,h = cv2.boundingRect(c)
     cv2.rectangle(im, (x,y),(x+w,y+h), (0,0,255), 3)
     if length <= 1.1*width or width >= 0.9*length:
       cv2.putText(im,"violation detected", (50, rows / 2 + 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255),2)
+    #cv2.imshow('r',im)
+    #cv2.imshow('thres',edged)
+    #cv2.waitKey(100)
     try:
       self.image_pub.publish(self.bridge.cv2_to_imgmsg(cvImage, "bgr8"))
     except CvBridgeError, e:
@@ -69,6 +74,7 @@ def main(args):
     rospy.spin()
   except KeyboardInterrupt:
     print "Shutting down"
+  cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main(sys.argv)
