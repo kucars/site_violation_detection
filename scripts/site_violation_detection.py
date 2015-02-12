f0fa6a280ae2fc875d95fdf4c896afdc0bba156c
Tarek Taha <tarek.taha@gmail.com>
2015-02-12 18:55:28 +0400

    - post competition checkpount

--- scripts/site_violation_detection.py
+++ scripts/site_violation_detection.py
@@ -26,7 +26,7 @@ lastTimeHImageSaved     = datetime.now()
 imagesFolder = ''
 imageCounter = 0
 #How often in seconds to analyse streamed images
-analysisTime = 5
+analysisTime = 2
 #How often in seconds to publish analysis results
 publishingTime = 1
 # Saving time
